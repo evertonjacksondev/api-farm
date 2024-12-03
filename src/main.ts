830c76372306e61c4d395248010be8836ffa22f5
Everton Jackson Oliveira Lopes <note-everton@Laptop-de-Everton.local>
2024-12-03 03:02:41 -0300

    fix: edit port

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/v1/docs', app, document);
-  await app.listen(8000);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(9000);
 }
 bootstrap();
